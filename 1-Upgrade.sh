05952ec9553d51ad4ab152216035cad611ed8e1d
meokgo <cimdieke@hotmail.com>
2024-10-31 15:17:15 -0500

    1-Upgrade.sh

---
+++ 1-Upgrade.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+#This script will upgrade the factory default state OS from Debian Buster to Debian Bullseye on UniFi Cloud Key Model: UCK-G2-PLUS
+#This script will disable or remove most UniFi packages, the device will no longer function as a Cloud Key for UniFi devices, but Emergency Recovery UI still works.
+#****It is highly recommended to factory reset the Cloud Key before running script the first time****
+#Factory reset Cloud Key: sudo ubnt-systool reset2defaults
+#Default SSH user: root
+#Default SSH password: ubnt
+#Download script: sudo wget https://raw.githubusercontent.com/meokgo/UC-CK/main/1-Combined-Upgrade.sh
+#Make script executable: sudo chmod +x 1-Combined-Upgrade.sh
+#Run script: sudo ./1-Combined-Upgrade.sh
